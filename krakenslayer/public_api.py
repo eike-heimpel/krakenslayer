e72a94e407f97382d918cc56d85d4afd2c73c1c3
Eike LALA <eike@heimpel.family>
2021-02-07 16:45:27 +0100

    first commit

--- krakenslayer/public_api.py
+++ krakenslayer/public_api.py
@@ -1,6 +1,5 @@
 import krakenex
-import logging
-import sys
+from pprint import pprint
 
 
 kraken = krakenex.API()
@@ -9,65 +8,35 @@ class Coin:
 
     def __init__(self, coin_ticker):
         self.coin_ticker = coin_ticker
-        self.asset_pair_ticker = f"{coin_ticker}USDT"
+        self.asset_pair = f"{coin_ticker}USDT"
         self.last_traded_price = None
-        self.minimum_volume = None
+        self.minimum_order_volume = None
+        self.maximum_trading_fee = None
+        self.lot_multiplier = None
 
-    def update_last_traded_price(self):
+    def update_asset_pair_infos(self):
 
-        asset_pair_ticker = kraken.query_public("Ticker", {"pair": self.asset_pair_ticker})
+        self._update_last_traded_price()
 
-        if "EQuery:Unknown asset pair" in asset_pair_ticker["error"]:
-            raise Exception(f"{self.asset_pair_ticker} asset pair does not exist")
+        asset_pair_info = kraken.query_public("AssetPairs", {"pair": self.asset_pair})
+        self._check_for_unknwown_asset_pair_error(asset_pair_info)
 
-        self.last_traded_price = asset_pair_ticker["result"][self.asset_pair_ticker]["c"][0]
-
-    def update_minimum_volume(self, asset_pair):
-
-        asset_pair_info = kraken.query_public("AssetPairs", {"pair": asset_pair})
-
-        if "EQuery:Unknown asset pair" in asset_pair_info["error"]:
-            raise Exception(f"{asset_pair} asset pair does not exist")
-
-        self.asset_pair_result = asset_pair_info["result"][asset_pair]
-
-    
-    def _check_for_unknwown_error(self):
-
-ether = Coin("ETH")
-print(ether.last_traded_price)
-ether.update_last_traded_price()
-print(ether.last_traded_price)
-
-exit()
-
-class PublicAPI:
-
-    def __init__(self):
-        self.kraken = krakenex.API()
-
-    def asset_pair_last_price(self, asset_pair):  # Sounds like asset should be a class
-
-        asset_pair_ticker = self.kraken.query_public("Ticker", {"pair": asset_pair})
-
-        if "EQuery:Unknown asset pair" in asset_pair_ticker["error"]:
-            raise Exception(f"{asset_pair} asset pair does not exist")
-
-        asset_price_euro = asset_pair_ticker["result"][asset_pair]["c"][0]
-
-        return asset_price_euro
-
-    def asset_pair_trading_minimum(self, asset_pair):
-
-        asset_pair_info = self.kraken.query_public("AssetPairs", {"pair": asset_pair})
-
-        if "EQuery:Unknown asset pair" in asset_pair_info["error"]:
-            raise Exception(f"{asset_pair} asset pair does not exist")
-
-        self.asset_pair_result = asset_pair_info["result"][asset_pair]
+        self.minimum_order_volume = asset_pair_info["result"][self.asset_pair]["ordermin"]
+        self.maximum_trading_fee = asset_pair_info["result"][self.asset_pair]["fees"][0][1]
+        self.lot_multiplier = asset_pair_info["result"][self.asset_pair]["lot_multiplier"]  # BEFORE EACH TRADE CHECK IF THE LOT MULTIPLIER IS 1 OTHERWISE DONT TRADE/ALERT. COULD COST MONEY
 
 
-p = PublicAPI()
+    def _update_last_traded_price(self):
 
-p.asset_pair_trading_minimum("XETHZEUR")
-print(p.asset_pair_result)
+        asset_pair_ticker = kraken.query_public("Ticker", {"pair": self.asset_pair})
+
+        self._check_for_unknwown_asset_pair_error(asset_pair_ticker)
+
+        self.last_traded_price = asset_pair_ticker["result"][self.asset_pair]["c"][0]
+
+
+    def _check_for_unknwown_asset_pair_error(self, api_asset_response):
+
+        if "EQuery:Unknown asset pair" in api_asset_response["error"]:
+            raise Exception(f"{api_asset_response} asset pair does not exist")
+
