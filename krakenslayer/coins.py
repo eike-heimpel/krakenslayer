261edee0e94d29bad50386943aad72e8f121631f
Eike LALA <eike@heimpel.family>
2021-02-07 18:11:30 +0100

    added trade updates

--- krakenslayer/coins.py
+++ krakenslayer/coins.py
@@ -4,16 +4,16 @@ kraken = krakenex.API()
 
 class Coin:
 
-    def __init__(self, coin_ticker):
+    def __init__(self, coin_ticker, fiat_currency):
         self.coin_ticker = coin_ticker
-        self.asset_pair = f"{coin_ticker}USDT"
+        self.asset_pair = f"{coin_ticker}{fiat_currency}"
         self.last_traded_price = None
         self.minimum_order_volume = None
         self.maximum_trading_fee = None
         self.lot_multiplier = None
-        self.amount_owned = None
+        self.amount_owned = 0
         self.average_purchase_price = None
-        self.
+        self.current_value = None
 
     def update_asset_pair_info(self):
 
@@ -25,6 +25,17 @@ class Coin:
         self.minimum_order_volume = asset_pair_info["result"][self.asset_pair]["ordermin"]
         self.maximum_trading_fee = asset_pair_info["result"][self.asset_pair]["fees"][0][1]
         self.lot_multiplier = asset_pair_info["result"][self.asset_pair]["lot_multiplier"]  # BEFORE EACH TRADE CHECK IF THE LOT MULTIPLIER IS 1 OTHERWISE DONT TRADE/ALERT. COULD COST MONEY
+
+
+    def update_with_new_trade(self, trade_info):
+
+        if trade_info["type"] == "buy":
+            self.amount_owned += float(trade_info["vol"])
+        elif trade_info["type"] == "sell":
+            self.amount_owned -= float(trade_info["vol"])
+
+        # calculate current value
+        # calculate average purchase price
 
 
     def _update_last_traded_price(self):
