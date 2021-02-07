5d678971f8424a17668eeccd3919cca5d666997b
Eike LALA <eike@heimpel.family>
2021-02-07 18:12:30 +0100

    will be deleted

--- public_examples.py
+++ public_examples.py
@@ -1,15 +1,38 @@
 from krakenslayer.coins import Coin
 from krakenslayer.record_keeping import My_Ledger
+import krakenex
+from pprint import pprint
 
 
 my_ledger = My_Ledger()
 
-ether = Coin("ETH")
-ada = Coin("ADA")
+ether = Coin("ETH", "EUR")
+ada = Coin("ADA", "EUR")
 
-my_ledger.add_asset("Etherium", ether)
-my_ledger.add_asset("Cardano", ada)
+my_ledger.add_asset(ether)
+my_ledger.add_asset(ada)
 
 ada.update_asset_pair_info()
 
-print(my_ledger.assets["Cardano"].last_traded_price)
+kraken = krakenex.API()
+kraken.load_key('kraken.key')
+
+
+## lets trade some
+
+order_ids = []
+# assume we placed multiple market orders here and added the ordertxid
+
+# now we need to check the trade history until we see all orders fulfilled so we can log them.
+trade_history = kraken.query_private("TradesHistory")["result"]
+
+for trade_id, trade in trade_history["trades"].items():
+    if trade["ordertxid"] in order_ids:
+        current_coin = my_ledger.assets[trade["pair"]]
+        my_ledger.add_trade(trade)
+        current_coin.update_with_new_trade(trade)
+
+        order_ids.remove(trade["ordertxid"])
+
+
+
